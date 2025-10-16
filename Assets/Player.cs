9064c6e3df2e0cea3dfcb97ed1829bbaf5bfd4e5
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-10-16 16:04:13 +0900

    Added jump feature

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
     // 公開する理由がない限りはprivateで調整できなくしておくのが基本（誤った調整操作を防ぐ）
     private Rigidbody2D rb;
     [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 8;
     private float xInput;
 
     private void Awake()
@@ -17,10 +18,32 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleInput();
+        HandleMovement();
+
+    }
+
+    private void HandleInput()
+    {
         // GetAxis: 徐々に-1, +1に近づくようになる
         xInput = Input.GetAxisRaw("Horizontal"); // 瞬時に-1, +1に変わる
 
+        // GetKey: 押している間 ...Down: 押した瞬間 ...Up: 離した瞬間
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+
+    }
+
+
+    private void HandleMovement()
+    {
         // -1 or +1 にするだけなので、Updateでやってしまって構わない。
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y); // 2次元ベクトル: Vector2
+    }
+
+    private void Jump()
+    {
+        Debug.Log("Someone pressed Jump() key");
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
     }
 }
