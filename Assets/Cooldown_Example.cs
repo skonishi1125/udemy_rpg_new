474b5e741646c6713e7d912244fdf9c67c2a5d6a
skonishi <73050501+skonishi1125@users.noreply.github.com>
2025-10-18 19:41:03 +0900

    251018 sec2 (#3)
    * added cooldown
    
    * learned the following:
    
    * Inheritance
    * Polymorphism
    
    * learned encapsulation

---
+++ Assets/Cooldown_Example.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class Cooldown_Example : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    [SerializeField] private float redColorDuration = 1;
+
+    public float currentTimeInGame;
+    public float lastTimeWasDamaged;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        ChangeColorIfNeeded();
+    }
+
+    private void ChangeColorIfNeeded()
+    {
+        currentTimeInGame = Time.time; // アプリケーションを起動してからの現在のフレームを秒単位で出す
+
+        if (currentTimeInGame > lastTimeWasDamaged + redColorDuration)
+        {
+            if (sr.color != Color.white)
+                sr.color = Color.white;
+        }
+    }
+
+    public void TakeDamage()
+    {
+        sr.color = Color.red;
+        lastTimeWasDamaged = Time.time;
+
+    }
+}
--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,8 +2,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    [SerializeField] protected string enemyName;
+    [SerializeField] protected float moveSpeed;
+
+    private void Update()
+    {
+        //moveAround();
+
+        if (Input.GetKeyDown(KeyCode.F))
+            Attack();
+    }
+
+    // health, armor, etc...
+
+    private void moveAround()
+    {
+        Debug.Log(enemyName + " moves at speed " + moveSpeed);
+    }
+
+    protected virtual void Attack()
+    {
+        Debug.Log(enemyName + " attacks!");
+    }
+
     public void TakeDamage()
     {
-        Debug.Log(gameObject.name + " took some damage!");
+
     }
+
+    public string GetEnemyName()
+    {
+        return enemyName;
+    }
+
 }
