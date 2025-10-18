8ae2934beb371c195a9090b69bc8e25d58d7bc91
SKONI_NOTE\watas <skonishi1125@gmail.com>
2025-10-18 17:30:15 +0900

    added cooldown

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -2,8 +2,38 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private SpriteRenderer sr;
+
+    [SerializeField] private float redColorDuration = 1;
+
+    public float currentTimeInGame;
+    public float lastTimeWasDamaged;
+
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        ChangeColorIfNeeded();
+    }
+
+    private void ChangeColorIfNeeded()
+    {
+        currentTimeInGame = Time.time; // アプリケーションを起動してからの現在のフレームを秒単位で出す
+
+        if (currentTimeInGame > lastTimeWasDamaged + redColorDuration)
+        {
+            if (sr.color != Color.white)
+                sr.color = Color.white;
+        }
+    }
+
     public void TakeDamage()
     {
-        Debug.Log(gameObject.name + " took some damage!");
+        sr.color = Color.red;
+        lastTimeWasDamaged = Time.time;
+
     }
 }
